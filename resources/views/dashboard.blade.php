65f801fd05f8990d78fd5608c045ef8dced3ae5a
lenicahyani <lenicahyani12tkj216@gmail.com>
2021-04-02 22:24:32 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+@section('title','Laravel')
+@section('content')
+<div class="section-body">
+</div>
+
+<div>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>
+    <a href="https://laravel.com/docs">Docs</a>    
+</div>
+
+@endsection
+
+@push('page-scripts')
+
+
+@endpush
